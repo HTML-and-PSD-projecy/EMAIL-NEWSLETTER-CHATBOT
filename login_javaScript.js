b344fce52b29da94933a7659cbecc8732d5b476d
Gundeep Singh <115575929+Gundeep-singh07@users.noreply.github.com>
2024-10-07 19:32:34 +0530

    Update login_javaScript.js

--- login_javaScript.js
+++ login_javaScript.js
@@ -1,27 +1,9 @@
-let signUpBtn = document.querySelector(".signupbtn");
-let signInBtn = document.querySelector(".signinbtn");
-let nameField = document.querySelector(".namefield");
-let title = document.querySelector(".title");
-let underline = document.querySelector(".underline");
-let text = document.querySelector(".text");
-let miss = document.querySelector(".miss");
-
-signInBtn.addEventListener("click", () => {
-  nameField.style.maxHeight = "0";
-  title.innerHTML = "Let's sign you in!";
-  text.innerHTML = "LOST PASSWORD ";
-  signUpBtn.classList.add("disable");
-  signInBtn.classList.remove("disable");
-  underline.style.transform = "translateX(35)";
-  miss.innerHTML = "Welcome back, You've been missed!";
+const container = document.querySelector(".login-container");
+const LoginLink = document.querySelector(".SignInLink");
+const RegisterLink = document.querySelector(".SignUpLink");
+RegisterLink.addEventListener("click", () => {
+  container.classList.add("active");
 });
-
-signUpBtn.addEventListener("click", () => {
-  nameField.style.maxHeight = "60px";
-  title.innerHTML = "Create new account";
-  text.innerHTML = "PASSWORD SUGGESTIONS ";
-  signUpBtn.classList.remove("disable");
-  signInBtn.classList.add("disable");
-  underline.style.transform = "translateX(0))";
-  miss.innerHTML = "Listen music for free!";
+LoginLink.addEventListener("click", () => {
+  container.classList.remove("active");
 });
